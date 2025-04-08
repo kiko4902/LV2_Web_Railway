364ecf63fb09d0c502f5193db0902220048360b3
Kristian Dzoic <kikodzoic@gmail.com>
2025-04-08 13:47:09 +0200

    affa

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const fs= require('fs');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-res.send('Pozdrav sa Railway servera!');
+
+app.get('/slike', (req, res)=> {
+    const folderPath = path.join(__dirname, 'public','images');
+    const files = fs.readdirSync(folderPath);
+    
+    const images = files
+    .filter(file=> file.endsWith('.jpg') || file.endsWith('.svg'))
+    .map((file,index) => ({
+        url: `/images/${file}`,
+        id: `slika${index + 1}`,
+        title: `Slika ${index +1}`
+}));
+    res.render('slike', { images});
 });
-app.listen(PORT, () => {
-console.log('Server pokrenut na portu ${PORT}');
-});
+
+ app.listen(PORT, () => {
+    console.log('Server pokrenut na portu ${PORT}');
+    });
+    
